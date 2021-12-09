f4253070bec3363702102ab20d2f6a7cc77cdd97
Lucy Joyce <lucyj20490@gmail.com>
2021-12-09 15:50:26 +0000

    Add stub navigation and profile picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
+import profileImage from "./lucy.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>
-         Lucy Joyce
-       </h1>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">About Me</div>
+        <div className="col">My Projects</div>
+        <div className="col">Contact Information</div>
+      </div>
+      <div className="PersonalInfo">
+        <h1>Lucy Joyce</h1>
+        <img className="Photo" src={profileImage} alt="Lucy Joyce" />
+        <p>Need to write here about skills etc</p>
+      </div>
     </div>
   );
 }
